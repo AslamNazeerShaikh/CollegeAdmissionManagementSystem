8dd23afbd6afc47e74915a81ecb35b3e41a5e3a0
BeingTiger007 <aslam.tiger.9047@gmail.com>
2021-02-04 01:46:56 +0530

    AppIcon Added , HomeScreen & CoursesScreen Completed

--- app/src/main/java/in/org/cocsit/collegeadmissionmanagementsystem/Courses.java
+++ app/src/main/java/in/org/cocsit/collegeadmissionmanagementsystem/Courses.java
@@ -1,23 +1,18 @@
 package in.org.cocsit.collegeadmissionmanagementsystem;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
 
-import com.daimajia.androidanimations.library.Techniques;
-import com.daimajia.androidanimations.library.YoYo;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class SplashScreen extends AppCompatActivity {
+public class Courses extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_courses);
 
         //add hardware acceleration enable
         getWindow().setFlags(
@@ -25,31 +20,9 @@ public class SplashScreen extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
 
         //add notch displays cutout mode to short size
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
         }
-
-        YoYo.with(Techniques.FadeInUp)
-                .duration(2000)
-                .repeat(0)
-                .playOn(findViewById(R.id.SplashScreenLogo));
-
-        YoYo.with(Techniques.FadeIn)
-                .duration(2000)
-                .repeat(0)
-                .playOn(findViewById(R.id.SplashScreenTitle));
-
-        YoYo.with(Techniques.FadeInUp)
-                .duration(2000)
-                .repeat(0)
-                .playOn(findViewById(R.id.SplashScreenSubTitle));
-
-        Handler handler = new Handler(); //handel the post delay method with intent to call next activity
-        handler.postDelayed(() -> {
-
-                startActivity(new Intent(SplashScreen.this,MainActivity.class));
-                finish();    //permanently closes the previous activity
-        },3000);  //value seconds //current value is 2sec
 
     }
 
--- app/src/main/java/in/org/cocsit/collegeadmissionmanagementsystem/MainActivity.java
+++ app/src/main/java/in/org/cocsit/collegeadmissionmanagementsystem/MainActivity.java
@@ -1,13 +1,24 @@
 package in.org.cocsit.collegeadmissionmanagementsystem;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
-public class MainActivity extends AppCompatActivity {
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +31,69 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
 
         //add notch displays cutout mode to short size
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
         }
 
+        CardView MainScreenRegistrationCard = findViewById(R.id.mainScreenRegistrationCard);
+        CardView MainScreenWalkthroughCard = findViewById(R.id.mainScreenWalkthroughCard);
+
+        MainScreenRegistrationCard.setOnClickListener(this); //added onclick listener
+        MainScreenWalkthroughCard.setOnClickListener(this);
+
+        ImageView showDialogButton = findViewById(R.id.mainScreenBackImgBtn);
+        showDialogButton.setOnClickListener(view -> showExitDialog());
+
+    }
+
+    public void showExitDialog() {
+
+        YoYo.with(Techniques.Shake)
+                .duration(2000)
+                .repeat(0)
+                .playOn(findViewById(R.id.mainScreenBackImgBtn));
+
+        Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.exit_alert_dialog);
+
+        Button acceptButton = dialog.findViewById(R.id.acceptButton);
+        Button cancelButton = dialog.findViewById(R.id.cancelButton);
+
+        acceptButton.setOnClickListener(view -> {
+            dialog.dismiss();
+            Log.e("Exit_App_Yes", "onClick: accept button");
+
+            Handler handler = new Handler(); //handel the post delay method with intent to call next activity
+            handler.postDelayed(() -> {
+                MainActivity.this.finish();
+                System.exit(0);
+            },2000);  //value seconds //current value is 2sec
+
+            Toast.makeText(getApplicationContext(), "You choose Yes action",Toast.LENGTH_SHORT).show();
+        });
+
+        cancelButton.setOnClickListener(view -> {
+            Log.e("Exit_App_No", "onClick: cancel button" );
+            dialog.dismiss();
+            Toast.makeText(getApplicationContext(), "You choose No action",Toast.LENGTH_SHORT).show();
+        });
+
+        dialog.show();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.mainScreenRegistrationCard:
+                Intent i = new Intent(this, Courses.class);
+                startActivity(i);
+                break;
+            case R.id.mainScreenWalkthroughCard:
+                Intent j = new Intent(this, SplashScreen.class);
+                startActivity(j);
+                break;
+        }
     }
 
     // Enables regular immersive mode or fullscreen mode
